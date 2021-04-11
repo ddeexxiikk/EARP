0e48ce702329620ba99a4fb203a52aba1cca3b4b
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-04-11 22:34:52 +0200

    zuzia profesjonalny debugger

--- odczytZbazy.py
+++ odczytZbazy.py
@@ -4,32 +4,26 @@ import datetime
 import requests
 
 def tcp():
-    try:
-        page = requests.get('https://github.com/ZuzGom/remote/blob/main/tcp.txt')
-    except requests.exceptions.ConnectionError:
-        linia = None
-    else:       
-        soup = BeautifulSoup(page.content, 'html.parser')
-        linia = str(soup.find("td", {"id": "LC1"})).split()[-1][9:-5]                        
-    return linia
+    fil = open('/home/pi/remote/tcp.txt')                      
+    return fil.readline()
 
-global connection=Null
+global connection
+connection=None
 
 #Function which connect with database
 def polaczenie():
     url = tcp().split(':')
     try:
         connection = connect(
-            host=url[1][2:],
-            port=url[2],
+            host='localhost',
+            #port=url[2],
             user="ul",
             password="earp123",
             database="Dane"
         )
         return connection
     except Error:
-        connection = Null
-        return connection
+        return None
     
 #Funkcja ktora odczytuje dane z bazy
 def execute_read_query(connection, query):
@@ -41,13 +35,16 @@ def execute_read_query(connection, query):
     except Error:
         result = None
         return result
+    
+global temp1, temp2, waga, AcceX, AcceY, AcceZ, RotX, RotY, RotZ, query2
 
-global temp1="0", temp2="0", waga="0", AcceX="0", AcceY="0", AcceZ="0", RotX="0", RotY="0", RotZ="0", query2="0"
+temp1,temp2, waga, AcceX, AcceY, AcceZ, RotX, RotY, RotZ, query2 = "0", "0", "0", "0", "0", "0", "0", "0", "0", "0000000000000000"
+
 
 def do_pliku():
     connection = polaczenie()
     
-    if(connection!=Null):
+    if(connection!=None):
         select_query = "SELECT temperature, AdditionalTemperature, Weight, AccelerationX, AccelerationY, AccelerationZ, RotationX, RotationY, RotationZ FROM Measurements ORDER BY Date"
         query1 = execute_read_query(connection, select_query)[-1]
         query2 = execute_read_query(connection, select_query)[-2]
@@ -74,6 +71,7 @@ def do_pliku():
         RotZ = str(query1[8])
     
     else:
+        print('brak polaczenia')
             
 do_pliku()
 
@@ -81,17 +79,6 @@ do_pliku()
 myfileLAST = open("DaneZBazyLAST.txt", "w")
 myfileLAST.write("" + temp1 + "\n" + temp2 + "\n" + waga + "\n" + AcceX + "\n" + AcceY + "\n" + AcceZ + "\n" + RotX + "\n" +  RotY + "\n" +  RotZ + "")
 myfileLAST.close()
-
-temp1="0" 
-temp2="0"
-waga="0"
-AcceX="0"
-AcceY="0"
-AcceZ="0"
-RotX="0"
-RotY="0"
-RotZ="0"
-query2="0"
 
 #Przedostatni zapis
 #Temperatura wewnatrz - temp1
--- zapisDObazy.py
+++ zapisDObazy.py
@@ -3,27 +3,22 @@ from mysql.connector import connect, Error
 import datetime
 from time import sleep
 from notify_run import Notify
+#import requests
 
 nf = Notify()
 
 #Very important function which help in connecting to database
 def tcp():
-    try:
-        page = requests.get('https://github.com/ZuzGom/remote/blob/main/tcp.txt')
-    except requests.exceptions.ConnectionError:
-        linia = None
-    else:       
-        soup = BeautifulSoup(page.content, 'html.parser')
-        linia = str(soup.find("td", {"id": "LC1"})).split()[-1][9:-5]                        
-    return linia
+    fil = open('/home/pi/remote/tcp.txt')                      
+    return fil.readline()
 
 #Function which connect with database
 def polaczenie():
     url = tcp().split(':')
     try:
         connection = connect(
-            host=url[1][2:],
-            port=url[2],
+            host='localhost',
+            #port=url[2],
             user="ul",
             password="earp123",
             database="Dane"
@@ -36,39 +31,3 @@ def polaczenie():
 #Funkcja przekazuje dane do bazy
 def execute_query(connection, query):
     cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-    except Error :
-        #Nic nie robi
-        a=0
-        
-def push_alert(id, error, tresc):
-    connection = polaczenie()
-    
-    if(connection!=None):
-        inserting_error = "INSERT INTO Alerty ( id, error, tekst ) VALUES ( " + str(id) + ", " + str(error) + ", \"" + tresc + "\" )"
-        execute_query(connection, inserting_error)
-    
-    connection.close()
-
-def alert(id_ul,code,message):
-    channel = 'admin'
-    url = 'http://127.0.0.1:5030/admin'
-    if id_ul == 1:
-        channel = '40CiRtPlbZUFnkHg'
-        url = 'https://notify.run/' + channel
-    
-    f = open('/home/pi/.config/notify-run', 'w')
-    f.write("{\"endpoint\": \""+str(url)+"\"}")
-    f.close()
-    nf.send(message)
-    push_alert(id_ul,code,message)
-
-myfile = open("KodyBledow.txt", "r")
-
-for x in myfile:
-    tresc = myfile.readline()
-    alert(1, 5, str(tresc))
-
-myfile.close()
