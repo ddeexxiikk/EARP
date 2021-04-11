5e89d8450c06422757444f27a12721e227bb3435
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-04-11 22:39:36 +0200

    Update zapisDObazy.py

--- zapisDObazy.py
+++ zapisDObazy.py
@@ -31,3 +31,35 @@ def polaczenie():
 #Funkcja przekazuje dane do bazy
 def execute_query(connection, query):
     cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+    except Error :
+        #Nic nie robi
+        a=0
+        
+def push_alert(id, error, tresc):
+    connection = polaczenie()
+    
+    if(connection!=None):
+        inserting_error = "INSERT INTO Alerty ( id, error, tekst ) VALUES ( " + str(id) + ", " + str(error) + ", \"" + tresc + "\" )"
+        execute_query(connection, inserting_error)
+    
+    connection.close()
+def alert(id_ul,code,message):
+    channel = 'admin'
+    url = 'http://127.0.0.1:5030/admin'
+    if id_ul == 1:
+        channel = '40CiRtPlbZUFnkHg'
+        url = 'https://notify.run/' + channel
+    
+    f = open('/home/pi/.config/notify-run', 'w')
+    f.write("{\"endpoint\": \""+str(url)+"\"}")
+    f.close()
+    nf.send(message)
+    push_alert(id_ul,code,message)
+myfile = open("KodyBledow.txt", "r")
+for x in myfile:
+    tresc = myfile.readline()
+    alert(1, 5, str(tresc))
+myfile.close()
